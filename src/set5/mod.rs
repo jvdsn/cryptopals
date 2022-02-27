fc2d32b16fdd8e3642793e0f96a8ccd0d4f2527a
Joachim Vandersmissen <git@jvdsn.com>
2022-02-27 15:22:45 +0100

    Challenge 36

--- src/set5/mod.rs
+++ src/set5/mod.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::shared::dh::{derive_shared, generate_keypair};
+    use crate::shared::dh::{derive_shared, generate_keypair, srp};
     use num_bigint::BigUint;
     use num_traits::{One, Zero};
     use std::ops::Sub;
@@ -18,13 +18,13 @@ mod tests {
         bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff\
         fffffffffffff";
         let g_hex = b"2";
-        let p = BigUint::parse_bytes(p_hex, 16).unwrap();
-        let g = BigUint::parse_bytes(g_hex, 16).unwrap();
+        let p = &BigUint::parse_bytes(p_hex, 16).unwrap();
+        let g = &BigUint::parse_bytes(g_hex, 16).unwrap();
 
-        let (a, A) = generate_keypair(&p, &g);
-        let (b, B) = generate_keypair(&p, &g);
-        let s1 = derive_shared(&p, &a, &B);
-        let s2 = derive_shared(&p, &b, &A);
+        let (ref a, ref A) = generate_keypair(p, g);
+        let (ref b, ref B) = generate_keypair(p, g);
+        let s1 = derive_shared(p, a, B);
+        let s2 = derive_shared(p, b, A);
         assert_eq!(s1, s2);
     }
 
@@ -43,29 +43,29 @@ mod tests {
         let g_hex = b"2";
 
         // Initialization
-        let Ap = BigUint::parse_bytes(p_hex, 16).unwrap();
-        let Ag = BigUint::parse_bytes(g_hex, 16).unwrap();
-        let (Aa, AA) = generate_keypair(&Ap, &Ag);
+        let Ap = &BigUint::parse_bytes(p_hex, 16).unwrap();
+        let Ag = &BigUint::parse_bytes(g_hex, 16).unwrap();
+        let (ref Aa, ref AA) = generate_keypair(Ap, Ag);
 
         // A->M
-        let Mp = Ap.clone();
-        let Mg = Ag.clone();
-        let _AB = AA.clone();
+        let Mp = Ap;
+        let Mg = Ag;
+        let _AB = AA;
 
         // M->B
-        let Bp = Mp.clone();
-        let Bg = Mg.clone();
-        let BA = Mp.clone();
-        let (Bb, BB) = generate_keypair(&Bp, &Bg);
+        let Bp = Mp;
+        let Bg = Mg;
+        let BA = Mp;
+        let (ref Bb, ref BB) = generate_keypair(&Bp, &Bg);
 
         // B->M
-        let _MB = BB.clone();
+        let _MB = BB;
 
         // M->A
-        let AB = Mp.clone();
+        let AB = Mp;
 
-        let As = derive_shared(&Ap, &Aa, &AB);
-        let Bs = derive_shared(&Bp, &Bb, &BA);
+        let As = derive_shared(Ap, Aa, AB);
+        let Bs = derive_shared(Bp, Bb, BA);
         assert!(As.is_zero());
         assert!(Bs.is_zero());
     }
@@ -84,36 +84,54 @@ mod tests {
         fffffffffffff";
         let g_hex = b"2";
 
-        let Ap = BigUint::parse_bytes(p_hex, 16).unwrap();
-        let Ag = BigUint::parse_bytes(g_hex, 16).unwrap();
+        let Ap = &BigUint::parse_bytes(p_hex, 16).unwrap();
+        let Ag = &BigUint::parse_bytes(g_hex, 16).unwrap();
 
-        let protocol = |g: BigUint| {
+        let protocol = |g: &BigUint| {
             // A -> M
-            let Mp = Ap.clone();
-            let _Mg = Ag.clone();
+            let Mp = Ap;
+            let _Mg = Ag;
 
             // M -> B
-            let Bp = Mp.clone();
+            let Bp = Mp;
             let Bg = g;
 
             // A -> B
-            let (Aa, AA) = generate_keypair(&Ap, &Ag);
-            let BA = AA.clone();
+            let (ref Aa, ref AA) = generate_keypair(Ap, Ag);
+            let BA = AA;
 
             // B -> A
-            let (Bb, BB) = generate_keypair(&Bp, &Bg);
-            let AB = BB.clone();
+            let (ref Bb, ref BB) = generate_keypair(Bp, Bg);
+            let AB = BB;
 
-            let As = derive_shared(&Ap, &Aa, &AB);
-            let Bs = derive_shared(&Bp, &Bb, &BA);
+            let As = derive_shared(Ap, Aa, AB);
+            let Bs = derive_shared(Bp, Bb, BA);
             (As, Bs)
         };
 
-        let (As, _Bs) = protocol(BigUint::one());
+        let (As, _Bs) = protocol(&BigUint::one());
         assert!(As.is_one());
-        let (As, _Bs) = protocol(Ap.clone());
+        let (As, _Bs) = protocol(Ap);
         assert!(As.is_zero());
-        let (As, _Bs) = protocol(Ap.clone().sub(1u8));
-        assert!(As.is_one() || As == Ap.clone().sub(1u8));
+        let (As, _Bs) = protocol(&Ap.sub(1u8));
+        assert!(As.is_one() || As == Ap.sub(1u8));
+    }
+
+    #[allow(non_snake_case)]
+    #[test]
+    fn test_challenge_36() {
+        let N_hex = b"\
+        ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024\
+        e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd\
+        3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec\
+        6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f\
+        24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361\
+        c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552\
+        bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff\
+        fffffffffffff";
+
+        let N = &BigUint::parse_bytes(N_hex, 16).unwrap();
+        let (CS, SS) = srp(N);
+        assert_eq!(CS, SS);
     }
 }
--- src/shared/dh.rs
+++ src/shared/dh.rs
@@ -1,4 +1,8 @@
-use num_bigint::{BigUint, RandBigInt};
+use crate::shared::random_bytes;
+use num_bigint::{BigUint, RandBigInt, ToBigInt};
+use num_integer::Integer;
+use sha2::digest::Digest;
+use sha2::Sha256;
 
 pub fn generate_keypair(p: &BigUint, g: &BigUint) -> (BigUint, BigUint) {
     let mut rng = rand::thread_rng();
@@ -9,4 +13,58 @@ pub fn generate_keypair(p: &BigUint, g: &BigUint) -> (BigUint, BigUint) {
 
 pub fn derive_shared(p: &BigUint, our_private: &BigUint, peer_public: &BigUint) -> BigUint {
     peer_public.modpow(our_private, p)
+}
+
+#[allow(non_snake_case)]
+pub fn srp(N: &BigUint) -> (BigUint, BigUint) {
+    let g_hex = b"2";
+    let k_hex = b"2";
+    let P = b"password";
+
+    let CN = N;
+    let Cg = &BigUint::parse_bytes(g_hex, 16).unwrap();
+    let Ck = &BigUint::parse_bytes(k_hex, 16).unwrap();
+    let (ref Ca, ref CA) = generate_keypair(N, Cg);
+
+    let SN = N;
+    let Sg = &BigUint::parse_bytes(g_hex, 16).unwrap();
+    let Sk = &BigUint::parse_bytes(k_hex, 16).unwrap();
+    let (ref Sb, ref SB) = generate_keypair(N, Cg);
+
+    let Ssalt = &random_bytes(16);
+    let mut sha256 = Sha256::new();
+    sha256.update(Ssalt);
+    sha256.update(P);
+    let Sx = &BigUint::from_bytes_be(sha256.finalize().as_slice());
+    let Sv = &Sg.modpow(Sx, SN);
+    let SB = &(Sk * Sv + SB);
+
+    let SA = CA;
+
+    let Csalt = Ssalt;
+    let CB = SB;
+
+    let mut sha256 = Sha256::new();
+    sha256.update(&SA.to_bytes_be());
+    sha256.update(&SB.to_bytes_be());
+    let Su = &BigUint::from_bytes_be(sha256.finalize().as_slice());
+
+    let mut sha256 = Sha256::new();
+    sha256.update(&CA.to_bytes_be());
+    sha256.update(&CB.to_bytes_be());
+    let Cu = &BigUint::from_bytes_be(sha256.finalize().as_slice());
+
+    let mut sha256 = Sha256::new();
+    sha256.update(Csalt);
+    sha256.update(P);
+    let Cx = &BigUint::from_bytes_be(sha256.finalize().as_slice());
+    let CS = (CB.to_bigint().unwrap() - (Ck * Cg.modpow(Cx, CN)).to_bigint().unwrap())
+        .mod_floor(&CN.to_bigint().unwrap())
+        .to_biguint()
+        .unwrap()
+        .modpow(&(Ca + Cu * Cx), CN);
+
+    let SS = (SA * Sv.modpow(Su, SN)).modpow(Sb, SN);
+
+    (CS, SS)
 }
